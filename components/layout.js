c9226853c671179b67f52eb077687ffe33b1201d
Matt Granmoe <mattgranmoe@gmail.com>
2017-10-17 13:06:23 -0500

    Deleted dead code

--- components/layout.js
+++ components/layout.js
@@ -3,18 +3,6 @@ import Link from 'next/link'
 import Head from './head'
 import styled from 'styled-components'
 
-const Navbar = styled.div`
-  display: flex;
-  align-items: space-around;
-  padding: 5px 10px;
-  background-color: black;
-  width: 100%;
-`
-
-const NavLinkWrapper = styled.div`
-  color: white;
-`
-
 const LINKS = [
   {
     name: 'About',
@@ -34,13 +22,6 @@ const LINKS = [
 export default ({ children, pageName }) => (
   <div>
     <Head title="Adopt a Zoo Animal" />
-    <Navbar>
-      { LINKS.map(({ name, href }) => (
-        <NavLinkWrapper key={ name }>
-          <Link href={ href }>{ name }</Link>
-        </NavLinkWrapper>
-      )) }
-    </Navbar>
     { children }
   </div>
 )
