8da4171f4db8c2d1214c0e24d38c1483d62c7595
Matt Granmoe <mattgranmoe@gmail.com>
2017-10-17 14:36:52 -0500

    Added home page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
 import Link from 'next/link'
 import Layout from '../components/layout'
+import Title from '../components/title'
 
 export default () => (
   <Layout>
+    <Title name="Welcome to the Zoo Adoption Center">
+      Thank you for your interest in adopting one of our lovely zoo animals.
+      Once adopted your animal will send you colorful postcards every month.
+    </Title>
   </Layout>
 )
+  
