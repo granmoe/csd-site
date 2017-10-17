b7e22b4d4efe603532a48ff8608496df1f7ecb1e
Matt Granmoe <mattgranmoe@gmail.com>
2017-10-17 14:39:58 -0500

    Tweaked about page

--- pages/about.js
+++ pages/about.js
@@ -1,17 +1,14 @@
 import Link from 'next/link'
 import Layout from '../components/layout'
+import Title from '../components/title'
 import { Button, Header, Form, Divider } from 'semantic-ui-react'
-
-const Title = ({ name }) => (
-  <div>
-    <Header as="h1">{ name }</Header>
-    <Divider />
-  </div>
-)
 
 export default () => (
   <Layout>
-    <Title name="About"/>
-    <div>We love our animals and we only take on those who have not been taken care of very well. We need your support to help our animals grow strong but most of all feel loved. They are waiting for you.</div>
+    <Title name="About">
+      We love our animals and we only take on those who have not been taken care of very well.
+      We need your support to help our animals grow strong but most of all feel loved.
+      They are waiting for you.
+    </Title>
   </Layout>
 )
