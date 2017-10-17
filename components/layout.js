15ad56e48acd09028b755c9eabb993cc4309a032
David Frenk <David.Frenk@irco.com>
2017-10-17 13:13:04 -0500

    Outline for Navigation

--- components/layout.js
+++ components/layout.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Link from 'next/link'
 import Head from './head'
 import styled from 'styled-components'
+import { Sidebar, Segment, Button, Menu, Image, Icon, Header } from 'semantic-ui-react'
 
 const LINKS = [
   {
@@ -19,9 +20,31 @@ const LINKS = [
   }
 ]
 
+
 export default ({ children, pageName }) => (
   <div>
     <Head title="Adopt a Zoo Animal" />
-    { children }
+	<Sidebar.Pushable as={Segment}>
+	<Sidebar as={Menu} animation='overlay' width='thin' visible={visible} icon='labeled' vertical inverted>
+	  <Menu.Item name='home'>
+		<Icon name='home' />
+		Home
+	  </Menu.Item>
+	  <Menu.Item name='gamepad'>
+		<Icon name='gamepad' />
+		Games
+	  </Menu.Item>
+	  <Menu.Item name='camera'>
+		<Icon name='camera' />
+		Channels
+	  </Menu.Item>
+	</Sidebar>
+	<Sidebar.Pusher>
+	  <Segment basic>
+		<Header as='h3'>Application Content</Header>
+		<Image src='/assets/images/wireframe/paragraph.png' />
+	  </Segment>
+	</Sidebar.Pusher>
+  </Sidebar.Pushable>  
   </div>
 )
